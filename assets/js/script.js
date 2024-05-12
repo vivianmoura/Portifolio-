35b9f60a97590ad76804aa7268356404c49a92a9
Vívian de Lima Moura <118912112+vivianmoura@users.noreply.github.com>
2024-05-12 13:38:16 -0300

    alterações form

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,6 +135,24 @@ for (let i = 0; i < formInputs.length; i++) {
   });
 }
 
+//MODAL
+
+function abrirModal(carregarModal){
+  // console.log("Carregar a janela modal:" + carregarModal);
+  let modal = document.getElementById(carregarModal);
+
+  modal.style.display = 'block';
+  // document.body.style.overflow = 'hidden';
+}
+
+function fecharModal(fecharModal){
+// console.log("Fechar a janela modal:" + fecharModal);
+
+let modal = document.getElementById(fecharModal);
+
+modal.style.display = 'none';
+}
+
 
 
 // page navigation variables
